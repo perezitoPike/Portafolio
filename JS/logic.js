a18ee8e0798406d29d0668e8331ea3694ae72d8e
perezitoPike <wilderchur@gmail.com>
2026-02-02 00:27:27 -0400

    UPD: index header section and About me section, ADD: styles in CSS folder, add styles to header and about me sections

---
+++ JS/logic.js
@@ -0,0 +1,14 @@
+// let lastScrollTop = 0;
+// const header = document.getElementById('main-header');
+
+// window.addEventListener('scroll', function () {
+//     let currentScroll = this.window.pageYOffset || this.document.documentElement.scrollTop;
+
+//     if (currentScroll > lastScrollTop) {
+//         header.classList.add('hidden');
+//     } else {
+//         header.classList.remove('hidden');
+//     }
+
+//     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+// }, false);
